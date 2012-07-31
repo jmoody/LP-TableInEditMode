9e2f8a807a5bb71c559cce797040d684fe98a1bd
Joshua Moody <joshuajmoody@gmail.com>
2012-07-31 09:09:00 +0200

    added a Gemfile

--- TableInEditMode/features/step_definitions/calabash_steps.rb
+++ TableInEditMode/features/step_definitions/calabash_steps.rb
@@ -5,25 +5,3 @@ Then /^I should be able to find the device orientation$/ do
   puts "the orientation = '#{orientation}'"
 end
 
-
-- (NSString *) deviceOrientation {
-  UIApplication *app = [UIApplication sharedApplication];
-  UIInterfaceOrientation orient =  app.statusBarOrientation;
-  switch (orient) {
-    case UIInterfaceOrientationLandscapeLeft:
-      return @"landscape left";
-      break;
-    case  UIInterfaceOrientationLandscapeRight:
-      return @"landscape right";
-      break;
-    case UIInterfaceOrientationPortrait:
-      return @"portrait";
-      break;
-    case UIInterfaceOrientationPortraitUpsideDown:
-      return @"portrait upside down";
-      break;
-    default:
-      return @"unknown orientation";
-      break;
-  }
-}
